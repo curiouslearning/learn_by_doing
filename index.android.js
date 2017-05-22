a0a7b1acf6fbf5db678730ea1eeb3d5af6378c7b
rye <micahrye@stardust.local>
2017-05-21 22:09:55 -0400

    finished, now just play around learn the code and practice debugging.

--- index.android.js
+++ index.android.js
@@ -33,40 +33,136 @@ const baseWidth = 1280;
 export default class LearnByDoing extends Component {
   constructor (props) {
     super(props);
-    console.log("I see I am in the constructor");
-  }
-  
-  componentWillMount () {
-    console.log("the component will mount");
+    this.state = {
+      iconArray: [],
+    };
+    const scaleWidth = screenWidth / baseWidth;
+    const scaleHeight = screenHeight / baseHeight;
+    this.scale = {
+      screenWidth: scaleWidth,
+      screenHeight: scaleHeight,
+      image: scaleHeight > scaleWidth ? scaleWidth : scaleHeight,
+    };
+
+    const iconList = [
+      {
+        name: 'BUBBLE',
+        imgSrc: require('./media/gameIcon/game7_icon_color.png'),
+        location: {top: 130, left: 100},
+        frameIndex: [13],
+      },
+    ];
+
+    this.iconList = _.shuffle(iconList);
+    this.iconAppearTimeout = [];
+    this.gameIcon = {tweenOptions: {}};
+    this.iconRefs = [];
+    this.initIcons();
   }
   
   componentDidMount () {
-    console.log("the component did mount");
+    _.forEach(this.iconList, (icon, index) => {
+      const timeout = setTimeout(() => {
+        debugger;
+        let iconRef = this.refs[this.iconRefs[index]];
+        iconRef.startTween();
+      }, 100 * index);
+      this.iconAppearTimeout.push(timeout);
+    });
+    
+  }
+
+  componentWillUnmount () {
+    _.forEach(this.iconAppearTimeout, timeout => clearTimeout(timeout));
   }
   
-  componentWillUnmount () {
-    console.log("say goodbye while you can");
+  startSize () {
+    return ({
+      width: 240 * this.scale.image,
+      height: 240 * this.scale.image,
+    });
+  }
+
+  scaleLocation (location) {
+    return ({
+      top: location.top * this.scale.screenHeight,
+      left: location.left * this.scale.screenWidth,
+    });
+
+  }
+
+  makeZoomTween (startScale=0.01, endScale= 1, duration=1000) {
+    //React bug (I think): Scale of 0 is set to 1 on load
+    if (startScale == 0) {
+      startScale = 0.01;
+    }
+    else if (endScale == 0) {
+      endScale == 0.01;
+    }
+    return ({
+      tweenType: "zoom-into-existence",
+      startScale: startScale,
+      startOpacity: 0,
+      endScale: endScale,
+      duration: duration,
+      loop: false,
+    });
+  }
+  
+  static navigationOptions = {
+    title: 'WELCOME HERE',
+    header: false
+  };
+  
+  press () {
+    const { navigate } = this.props.navigation;
+    console.log("PRESS PRESS");
+    navigate('Prefs');
+  }
+  
+  initIcons () {
+    this.icons = _.map(this.iconList, (icon, index) => {
+      const ref = ("gameRef" + index);
+      this.iconRefs.push(ref);
+      console.log("REFS = ", ref);
+      return (<AnimatedSprite
+        key={index}
+        ref={ref}
+        sprite={gameIcon}
+        animationFrameIndex={icon.frameIndex}
+        loopAnimation={false}
+        coordinates={{top:icon.location.top, left: icon.location.left}}
+        size={this.startSize()}
+        draggable={true}
+        scale={0.1}
+        opacity={0}
+        tweenOptions = {this.makeZoomTween(0.1, 1, 1000)}
+        tweenStart={'fromMethod'}
+        onPress={() => this.press()}
+      />);
+    });
   }
   
   render() {
-    console.log("this thing is going to render now")
     const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View
+        style={{ flex: 1, }}
+      >
         <Button
           onPress={() => navigate('Prefs')}
-          title="Go to Prefs!!!"
+          title="Go to Prefs"
         />
+        <Image
+          source={require('./media/backgrounds/back01.jpg')}
+          style={{
+            opacity: 1,
+            width: 1280,
+            height: 800,
+          }}
+        />
+      {this.icons}
+        
       </View>
     );
   }
