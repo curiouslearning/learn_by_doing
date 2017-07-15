a67bf68e424bb467ea5105f7a7e2de6cc0f8620c
micahrye <micahrye@gmail.com>
2017-07-15 00:09:13 -0400

    added example and comments on removing a task that has been queued.

--- js/async/learn_async.js
+++ js/async/learn_async.js
@@ -16,7 +16,7 @@ const task_runner = function (task, done) {
 
 // when number concurent tasks 1 it effectivly runs async code sequentially
 // change value and run code to see different behavior. 
-const number_concurent_tasks = 6; 
+const number_concurent_tasks = 1; 
 const task_queue = async.queue(task_runner, number_concurent_tasks);
 
 task_queue.drain = function() {
@@ -29,3 +29,13 @@ task_queue.push({name: "tcp", data: 2});
 task_queue.push({name: "pause", data: -2});
 task_queue.push({name: "tcp", data: 3});
 task_queue.push({name: "pause", data: -3});
+
+/** 
+* Removing task from Quey
+* remove is passed an objec with a data property that contains the tasks that
+* where pushed in. It expects a test, if true the item will be removed. 
+*/
+// task_queue.remove((queued_task) => {
+//   console.log(`task = ${JSON.stringify(queued_task.data)}`);
+//   return queued_task.data.data === -2;
+// });
